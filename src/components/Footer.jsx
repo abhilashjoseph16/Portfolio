fb3c2cb2d4aa303cbeac2f6d7c73c4a5490db1cf
abhilashjoseph16 <abhilash.pannikottil@gmail.com>
2025-09-01 21:32:19 +0530

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <div className='portfolio-footer-main-container'>
-
+    <div className="portfolio-footer-main-container">
+      <div className="footer-line"></div>
+      <p>
+        © {new Date().getFullYear()} Abhilash Joseph. All rights reserved. Made
+        with 🧡 and lots of coffee.
+      </p>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
